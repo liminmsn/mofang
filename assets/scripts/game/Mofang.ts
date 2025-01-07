3197cc6208aabc1992a9a76157f700923920e2fa
杨再涛 <1772081518@qq.com>
2025-01-07 18:15:22 +0800

    事件绑定

--- assets/scripts/game/Mofang.ts
+++ assets/scripts/game/Mofang.ts
@@ -1,11 +1,12 @@
-import { _decorator, EventMouse, EventTouch, NodeEventType, Vec2 } from 'cc';
+import { _decorator, EventMouse, EventTouch, NodeEventType, Vec2, Node } from 'cc';
 import { GameNodeBase } from '../lib/GameNodeBase';
-const { ccclass, property } = _decorator;
+const { ccclass, type } = _decorator;
 
-@ccclass('Mofang')
+@ccclass
 export class Mofang extends GameNodeBase {
+    @type(Node)
+    mf: Node;
     init_pos: Vec2;
-
     start() {
         console.log('my mf');
         this.OnAll([NodeEventType.MOUSE_DOWN, NodeEventType.TOUCH_START], this.Down);
@@ -13,6 +14,9 @@ export class Mofang extends GameNodeBase {
     Down(ent: EventMouse | EventTouch) {
         this.init_pos = ent.getLocationInView();
         this.OnAll([NodeEventType.MOUSE_MOVE, NodeEventType.TOUCH_MOVE], this.Move);
+        this.OnAll([NodeEventType.MOUSE_UP, NodeEventType.TOUCH_END], () => {
+            
+        });
     }
 
     Move(ent: EventTouch) {
