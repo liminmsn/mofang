2f1a833223d34bb00b44bbdc1689c58aec2d5a2b
杨再涛 <1772081518@qq.com>
2025-01-08 18:30:42 +0800

    旋转魔方逻辑编写

--- assets/scripts/ui/Control.ts
+++ assets/scripts/ui/Control.ts
@@ -1,4 +1,4 @@
-import { _decorator, EventMouse, EventTouch, NodeEventType, Vec2, Node, NodeSpace, v3, Slider, v2 } from 'cc';
+import { _decorator, EventMouse, EventTouch, NodeEventType, Vec2, Node, NodeSpace, v3, Slider, v2, Label } from 'cc';
 import { GameNodeBase } from '../lib/GameNodeBase';
 import { Cube } from '../game/Cube';
 const { ccclass, type } = _decorator;
@@ -7,8 +7,11 @@ const { ccclass, type } = _decorator;
 export class Control extends GameNodeBase {
     @type(Cube)
     cobe: Cube;
+    @type([Slider])
+    slider_arr: Slider[];
 
     start() {
+        this.run = true;
         this.OnAll([NodeEventType.MOUSE_DOWN, NodeEventType.TOUCH_START], this.Down);
     }
     Down(ent: EventMouse | EventTouch) {
@@ -17,6 +20,7 @@ export class Control extends GameNodeBase {
         //取消移动事件
         this.OnAll([NodeEventType.MOUSE_UP, NodeEventType.TOUCH_END], () => {
             this.OffAll([NodeEventType.MOUSE_MOVE, NodeEventType.TOUCH_MOVE]);
+            this.run = false;
         });
     }
 
@@ -24,20 +28,22 @@ export class Control extends GameNodeBase {
         const { x, y } = ent.getDelta();
     }
 
-    dir_: Vec2 = v2(0, 0);
-    OnTabScrll(ent: Slider, dir: string) {
-        if (ent.progress > 0.5) {
-            this.dir_[dir] = 1;
-        } else if (ent.progress < 0.5) {
-            this.dir_[dir] = -1;
-        } else {
-            this.dir_[dir] = 0;
-        }
-    }
-
+    run: boolean = false;
     protected update(dt: number): void {
-        this.cobe.rotaryY(this.dir_.x * dt);
-        this.cobe.rotaryX(this.dir_.y * dt);
+        if (this.run) {
+            this.slider_arr.forEach(item => {
+                const name = item.node.children[0].getComponent(Label).string;
+                if (item.progress != 0.5) {
+                    const dir_val = item.progress > 0.5 ? 1 : -1;
+                    if (name === 'X') {
+                        this.cobe.rotaryY(dir_val * dt);
+                    }
+                    if (name === 'Y') {
+                        this.cobe.rotaryY(dir_val * dt);
+                    }
+                }
+            });
+        }
     }
 }
 
